ca31503b7d28eb36d52a5c852a6c7b6cb40d27ff
Paul Choi <paulsoonminchoi@gmail.com>
2020-12-22 21:19:51 -0800

    Added new buttons for home page and changed background

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -30,9 +30,9 @@ const Footer = () => {
           </form>
         </div>
       </section>
-      <div className='footer-links'>
+      <div class='footer-links'>
         <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
+          <div class='footer-link-items'>
             <h2>About Us</h2>
             <Link to='/sign-up'>How it works</Link>
             <Link to='/'>Testimonials</Link>
@@ -40,7 +40,7 @@ const Footer = () => {
             <Link to='/'>Investors</Link>
             <Link to='/'>Terms of Service</Link>
           </div>
-          <div className='footer-link-items'>
+          <div class='footer-link-items'>
             <h2>Contact Us</h2>
             <Link to='/'>Contact</Link>
             <Link to='/'>Support</Link>
@@ -49,14 +49,14 @@ const Footer = () => {
           </div>
         </div>
         <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
+          <div class='footer-link-items'>
             <h2>Videos</h2>
             <Link to='/'>Submit Video</Link>
             <Link to='/'>Ambassadors</Link>
             <Link to='/'>Agency</Link>
             <Link to='/'>Influencer</Link>
           </div>
-          <div className='footer-link-items'>
+          <div class='footer-link-items'>
             <h2>Social Media</h2>
             <Link to='/'>Instagram</Link>
             <Link to='/'>Facebook</Link>
@@ -65,54 +65,54 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <section className='social-media'>
-        <div className='social-media-wrap'>
-          <div className='footer-logo'>
+      <section class='social-media'>
+        <div class='social-media-wrap'>
+          <div class='footer-logo'>
             <Link to='/' className='social-logo'>
               <Logo src='images/blulogoalone.png' alt='blulogo' className='Logo'></Logo>
             </Link>
           </div>
-          <small className='website-rights'>Blu © 2020</small>
-          <div className='social-icons'>
+          <small class='website-rights'>Blu © 2020</small>
+          <div class='social-icons'>
             <Link
-              className='social-icon-link facebook'
+              class='social-icon-link facebook'
               to='/'
               target='_blank'
               aria-label='Facebook'
             >
-              <i className='fab fa-facebook-f' />
+              <i class='fab fa-facebook-f' />
             </Link>
             <Link
-              className='social-icon-link instagram'
+              class='social-icon-link instagram'
               to='/'
               target='_blank'
               aria-label='Instagram'
             >
-              <i className='fab fa-instagram' />
+              <i class='fab fa-instagram' />
             </Link>
             <Link
-              className='social-icon-link youtube'
+              class='social-icon-link youtube'
               to='/'
               target='_blank'
               aria-label='Youtube'
             >
-              <i className='fab fa-youtube' />
+              <i class='fab fa-youtube' />
             </Link>
             <Link
-              className='social-icon-link twitter'
+              class='social-icon-link twitter'
               to='/'
               target='_blank'
               aria-label='Twitter'
             >
-              <i className='fab fa-twitter' />
+              <i class='fab fa-twitter' />
             </Link>
             <Link
-              className='social-icon-link twitter'
+              class='social-icon-link twitter'
               to='/'
               target='_blank'
               aria-label='LinkedIn'
             >
-              <i className='fab fa-linkedin' />
+              <i class='fab fa-linkedin' />
             </Link>
           </div>
         </div>
