7df7d9c36452532c7bdf8208f2e912f60016bdb3
Paul Choi <paulsoonminchoi@gmail.com>
2021-01-16 18:23:51 -0800

    Added new button animations to hide mountains and circles

--- client/src/components/HeroSection.js
+++ client/src/components/HeroSection.js
@@ -1,7 +1,7 @@
 import './HeroSection.css';
 import '../App.css';
-import React, { useEffect, useRef } from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect, useRef } from 'react';
+import styled, { keyframes } from 'styled-components';
 import Button from './subComponents/Button.js';
 import Button2 from './subComponents/Button2.js';
 import { gsap } from 'gsap';
@@ -12,10 +12,37 @@ const LengthOfButton = styled.div`
   width: 250px;
 `;
 
+const Polygon = styled.div`
+  opacity: ${(props) => props.show ? 0 : 1};
+  transition: all 1s ease;
+`;
+
+const Circle = styled.div`
+  opacity: ${(props) => props.show ? 0 : 1};
+  transition: all 1s ease;
+`;
+
+const NewTeamText = styled.span`
+  color: white;
+  font-family: SF Pro Display;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 25px;
+  line-height: 40px;
+  transition: all 3s ease;
+  opacity: ${(props) => props.show ? 1 : 0};
+`;
+
+const DemoVidText = styled(NewTeamText)`
+  color: black;
+`;
+
 const HeroSection = () => {
 
   let button1 = useRef(null);
   let button2 = useRef(null);
+  const [showMountains, setShowMountains] = useState(false);
+  const [showCircles, setShowCircles] = useState(false);
 
   useEffect(() => {
     gsap.from([button1, button2], 0.8, {
@@ -31,38 +58,42 @@ const HeroSection = () => {
 
   return (
     <div className='hero-container'>
+        <Polygon show={showMountains} className="polygon1"></Polygon>
+        <Polygon show={showMountains} className="polygon2"></Polygon>
+        <Polygon show={showMountains} className="polygon3"></Polygon>
+        <Polygon show={showMountains} className="polygon4"></Polygon>
+        <Circle show={showCircles} className="ellipse20"></Circle>
+        <Circle show={showCircles} className="ellipse22"></Circle>
+        <Circle show={showCircles} className="ellipse21"></Circle>
+        <Circle show={showCircles} className="ellipse23"></Circle>
+        <Circle show={showCircles} className="ellipse24"></Circle>
+        <Circle show={showCircles} className="ellipse25"></Circle>
+        <div className="create-new-team-text-container">
+          <NewTeamText show={showMountains} >Create a Valorant Team and start inviting your friends to build a roster. You can also search for friends / players to join your team based on roles / characters.</NewTeamText>
+        </div>
       <div className='hero-btns'>
         <div className="hero-content-containter">
-          <span className="hero-text">To get started click "Get Started" to create an account with Blu!</span>
           <div className='button-containers'>
             <div className="line-wrap" >
               <div ref={el => (button1 = el)}>
-                <Button2 location={'sign-up'} onClick={() => console.log('hello')} text={'Get Started'}></Button2>
+                <Button2 location={'sign-up'} text={'Create new Team'} onMouseOver={() => setShowMountains(true)}></Button2>
                 <LengthOfButton></LengthOfButton>
               </div>
             </div>
             <div className="line-wrap" >
               <div ref={el => (button2 = el)} >
-                <Button2 location={'sign-up'} onClick={() => console.log('hello!!!!')} text={'Watch Demo'}></Button2>
+                <Button2 location={'sign-up'} onMouseOver={() => setShowCircles(true)} text={'Watch Demo'}></Button2>
                 <LengthOfButton></LengthOfButton>
               </div>
             </div>
           </div>
-          <span className="hero-text">To see a preview of what Blu is all about, click "Watch Demo" to watch a live demo.</span>
         </div>
       </div>
+      <div className="create-new-team-text-container">
+          <DemoVidText show={showCircles} >To find out how to use Blu, watch the demo video provided to learn how to get started and begin climbing the leaderboards today!</DemoVidText>
+        </div>
     </div>
   )
 }
 
 export default HeroSection;
-
-
-{/* <div className='hero-btns'>
-          <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large' >
-              Get Started
-            </Button>
-            <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large' >
-              Watch Demo
-            </Button>
-        </div> */}
