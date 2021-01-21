ab5655614de492ed523804f5846c4ec3c9c821bd
Paul Choi <paulsoonminchoi@gmail.com>
2021-01-20 21:26:30 -0800

    Added valorant backgroundground witd with mobile friendly responsiveness

--- client/src/components/HeroSection.js
+++ client/src/components/HeroSection.js
@@ -1,8 +1,7 @@
 import './HeroSection.css';
 import '../App.css';
 import React, { useState, useEffect, useRef } from 'react';
-import styled, { keyframes } from 'styled-components';
-import Button from './subComponents/Button.js';
+import styled from 'styled-components';
 import Button2 from './subComponents/Button2.js';
 import { gsap } from 'gsap';
 
@@ -10,35 +9,6 @@ import { gsap } from 'gsap';
 
 const LengthOfButton = styled.div`
   width: 250px;
-`;
-
-const Cloud = styled.div`
-  top: ${(props) => props.show ? '60%' : '40%'};
-  transition: all 1s ease;
-`;
-
-const Cloud2 = styled(Cloud)`
-  top: ${(props) => props.show ? '68%' : '38%'};
-  left: ${(props) => props.show ? '20%' : '25%'};
-`;
-
-const Cloud3 = styled(Cloud)`
-  top: ${(props) => props.show ? '20%' : '25%'};
-`;
-
-const Polygon = styled.div`
-  top: ${(props) => props.show ? '60%' : '43%'};
-  transition: all 1s ease;
-`;
-
-const Circle = styled.div`
-  top: ${(props) => props.show ? '15%' : '25%'};
-  transition: all 1s ease;
-`;
-
-const Circle2 = styled(Circle)`
-  top: ${(props) => props.show ? '53%' : '27%'};
-  left: ${(props) => props.show ? '64%' : '75%'};
 `;
 
 const NewTeamText = styled.span`
@@ -78,23 +48,12 @@ const HeroSection = () => {
 
   return (
     <div className='hero-container'>
-        {/* <Cloud show={showMountains} className="cloud1"></Cloud>
-        <Cloud2 show={showMountains} className="cloud2"></Cloud2>
-        <Cloud3 show={showMountains} className="cloud3"></Cloud3>
-        <div className="polygon1"></div>
-        <div className="polygon2"></div>
-        <Polygon show={showMountains} className="polygon3"></Polygon>
-        <div className="polygon4"></div>
-        <div className="ellipse20"></div>
-        <div className="ellipse22"></div>
-        <div className="ellipse21"></div>
-        <Circle show={showCircles} className="ellipse23"></Circle>
-        <div className="ellipse24"></div>
-        <div className="ellipse25"></div>
-        <Circle2 show={showCircles} className="ellipse26"></Circle2> */}
-        <div className="create-new-team-text-container">
+        <div className="valorant-background">
+          <div className="valorant-background-cover"></div>
+      </div>
+        {/* <div className="create-new-team-text-container">
           <NewTeamText show={showMountains} >Create a Valorant Team and start inviting your friends to build a roster. You can also search for friends / players to join your team based on roles / characters.</NewTeamText>
-        </div>
+        </div> */}
       <div className='hero-btns'>
         <div className="hero-content-containter">
           <div className='button-containers'>
@@ -113,9 +72,9 @@ const HeroSection = () => {
           </div>
         </div>
       </div>
-      <div className="create-new-team-text-container">
+      {/* <div className="create-new-team-text-container">
           <DemoVidText show={showCircles} >To find out how to use Blu, watch the demo video provided to learn how to get started and begin climbing the leaderboards today!</DemoVidText>
-        </div>
+        </div> */}
     </div>
   )
 }
