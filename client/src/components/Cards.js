03de1b857738253409871442eff086ac2af27f5b
Paul Choi <paulsoonminchoi@gmail.com>
2021-01-21 20:50:32 -0800

    Removed old unused svgs and adding more styling to title of team cards section

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -2,15 +2,27 @@ import React from 'react';
 import './Cards.css';
 import CardItem from './CardItem.js';
 import styled from 'styled-components';
+import smallBorder from '../images/smallborder.svg';
 
 const Title = styled.h1`
-  color: black;
+  position: absolute;
+  color: #F5DBE2;
   font-family: SF Pro Display;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 30px;
+  font-style: italic;
+  font-weight: 600;
+  font-size: 45px;
   line-height: 40px;
   text-align: center;
+  z-index: 31;
+  left: 30.5%;
+  top: 2%;
+  text-shadow: 0px 15px 5px rgba(0,0,0,0.1),
+  10px 20px 5px rgba(0,0,0,0.05),
+  -10px 20px 5px rgba(0,0,0,0.05);
+  background: url(${smallBorder}) no-repeat;
+  height: 80px;
+  width: 660px;
+  padding: 15px;
 `;
 
 const Cards = () => {
@@ -20,48 +32,48 @@ const Cards = () => {
         Newly Created Valorant Teams
       </Title>
       <div className='cards__container'>
-        <div className='cards__wrapper'>
-          <ul className='cards__items'>
-            <CardItem
-              src='images/nrg.jpg'
-              text='Energy Gaming'
-              label='NRG'
-              path='/gamelobby'
-            />
-            <CardItem
-              src='images/blu.png'
-              text='Buddies Like U'
-              label='BLU'
-              path='/gamelobby'
-            />
-            <CardItem
-              src='images/c9.jpg'
-              text='Cloud Gaming'
-              label='C9'
-              path='/gamelobby'
-            />
-          </ul>
-          <ul className='cards__items'>
-            <CardItem
-              src='images/TL.png'
-              text='Team Liquid'
-              label='TL'
-              path='/gamelobby'
-            />
-            <CardItem
-              src='images/faze.jpg'
-              text='Faze Clan'
-              label='FC'
-              path='/gamelobby'
-            />
-            <CardItem
-              src='images/100t.jpg'
-              text='100 Thieves'
-              label='100t'
-              path='/gamelobby'
-            />
-          </ul>
-        </div>
+          <div className='cards__wrapper'>
+            <ul className='cards__items'>
+              <CardItem
+                src='images/nrg.jpg'
+                text='Energy Gaming'
+                label='NRG'
+                path='/gamelobby'
+              />
+              <CardItem
+                src='images/blu.png'
+                text='Buddies Like U'
+                label='BLU'
+                path='/gamelobby'
+              />
+              <CardItem
+                src='images/c9.jpg'
+                text='Cloud Gaming'
+                label='C9'
+                path='/gamelobby'
+              />
+            </ul>
+            <ul className='cards__items'>
+              <CardItem
+                src='images/TL.png'
+                text='Team Liquid'
+                label='TL'
+                path='/gamelobby'
+              />
+              <CardItem
+                src='images/faze.jpg'
+                text='Faze Clan'
+                label='FC'
+                path='/gamelobby'
+              />
+              <CardItem
+                src='images/100t.jpg'
+                text='100 Thieves'
+                label='100t'
+                path='/gamelobby'
+              />
+            </ul>
+          </div>
       </div>
     </div>
   )
