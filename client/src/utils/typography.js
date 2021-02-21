eb8d9ac280091a3a458ff97faa31987369de796c
Paul Choi <paulsoonminchoi@gmail.com>
2021-02-20 16:46:10 -0800

    Completed adding typescale and new fonts to signup page

---
+++ client/src/utils/typography.js
@@ -0,0 +1,14 @@
+export const primaryFont = '"Bebas Neue", cursive';
+
+export const secondayFont = '"Roboto", sans-serif';
+
+export const typeScale = {
+  header1: "5.00rem",
+  header2: "2.441rem",
+  header3: "1.953rem",
+  header4: "1.563rem",
+  header5: "1.25rem",
+  paragraph: "1rem",
+  helperText: "0.8rem",
+  copyRightText: "0.64rem"
+}
